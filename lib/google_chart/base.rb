570e70477ac137a614cfa54d30adaaac30aeb8b7
Xavier Shay <xavier@rhnh.net>
2010-02-25 16:31:31 +1100

    You can customize the base_url

--- lib/google_chart/base.rb
+++ lib/google_chart/base.rb
@@ -3,7 +3,7 @@ require 'uri'
 module GoogleChart
   class Base
     BASE_URL = "http://chart.apis.google.com/chart?"
-    
+
     SIMPLE_ENCODING = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
     COMPLEX_ENCODING_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.'.split('');
     @@complex_encoding = []
@@ -12,7 +12,7 @@ module GoogleChart
         @@complex_encoding[index_outer * 64 + index_inner] = outer + inner
       end
     end
-    
+
     SHAPE_MARKERS = {:arrow => "a",
       :cross => "c",
       :diamond => "d",
@@ -27,29 +27,32 @@ module GoogleChart
     DEFAULT_LINE_STYLE = '1'
 
     # Size of the chart in WIDTHxHEIGHT format
-    attr_accessor :chart_size 
-    
+    attr_accessor :chart_size
+
     # Type of the chart. Usually, you do not need to set this yourself
-    attr_accessor :chart_type 
-    
+    attr_accessor :chart_type
+
     # Chart title
     attr_accessor :chart_title
-    
+
     # RRGGBB hex value for the color of the title
     attr_accessor :title_color
-    
+
     # Font size of the title
     attr_accessor :title_font_size
-    
+
     # Data encoding to use. Can be one of <tt>:simple</tt>, <tt>:text</tt> or <tt>:extended</tt> (see http://code.google.com/apis/chart/#chart_data)
     attr_accessor :data_encoding
-    
-    # A hash of the params used to construct the URL        
+
+    # A hash of the params used to construct the URL
     attr_accessor :params
-    
+
     # Set to <tt>true</tt> or <tt>false</tt> to show or hide the chart legend. Not applicable for Scatter Chart.
     attr_accessor :show_legend
-    
+
+    # Where to send requests, you can customize for proxying requests through your own server.
+    attr_accessor :base_url
+
     def initialize(chart_size, chart_title)
       self.params = Hash.new
       @labels = []
@@ -62,8 +65,9 @@ module GoogleChart
       self.chart_title   = chart_title
       self.data_encoding = :simple
       self.show_legend   = true
+      self.base_url      = BASE_URL
     end
-    
+
     # Generates the URL string that can be used to retrieve the graph image in PNG format.
     # Use this after assigning all the properties to the graph
     # You can pass in additional params as a hash for features that may not have been implemented
@@ -72,12 +76,12 @@ module GoogleChart
     #      lc.data "Trend 1", [5,4,3,1,3,5,6], '0000ff'
     #      lc.data "Trend 2", [1,2,3,4,5,6], '00ff00'
     #      lc.data "Trend 3", [6,5,4,3,2,1], 'ff0000'
-    #      puts lc.to_url({:chm => "000000,0,0.1,0.11"}) # Single black line as a horizontal marker        
+    #      puts lc.to_url({:chm => "000000,0,0.1,0.11"}) # Single black line as a horizontal marker
     def to_url(extras={})
       prepare_params
       params.merge!(extras)
       query_string = params.map { |k,v| "#{k}=#{URI.escape(v.to_s).gsub(/%20/,'+').gsub(/%7C/,'|')}" }.join('&')
-      BASE_URL + query_string
+      base_url + query_string
     end
 
     # Generates a fully encoded URL string that can be used to retrieve the graph image in PNG format.
@@ -91,20 +95,20 @@ module GoogleChart
     #      lc.data "Trend 1", [5,4,3,1,3,5,6], '0000ff'
     #      lc.data "Trend 2", [1,2,3,4,5,6], '00ff00'
     #      lc.data "Trend 3", [6,5,4,3,2,1], 'ff0000'
-    #      puts lc.to_escaped_url({:chm => "000000,0,0.1,0.11"}) # Single black line as a horizontal marker            
+    #      puts lc.to_escaped_url({:chm => "000000,0,0.1,0.11"}) # Single black line as a horizontal marker
     def to_escaped_url(extras={})
       prepare_params
       params.merge!(extras)
       query_string = params.map { |k,v| "#{k}=#{URI.escape(v.to_s)}" }.join('&')
-      BASE_URL + query_string
+      base_url + query_string
     end
-    
+
     # Adds the data to the chart, according to the type of the graph being generated.
     #
     # [+name+] is a string containing a label for the data.
     # [+value+] is either a number or an array of numbers containing the data. Pie Charts and Venn Diagrams take a single number, but other graphs require an array of numbers
     # [+color+ (optional)] is a hexadecimal RGB value for the color to represent the data
-    # 
+    #
     # ==== Examples
     #
     # for GoogleChart::LineChart (normal)
@@ -121,12 +125,12 @@ module GoogleChart
       @labels << name
       @colors << color if color
     end
-    
+
     # Allows (optional) setting of a max value for the chart, which will be used for data encoding and axis plotting.
     # The value to pass depends on the type of chart
     # * For Line Chart and Bar Charts it should be a single integer or float value
     # * For Scatter Charts and Line XY Charts, you MUST pass an array containing the maximum values for X and Y
-    # 
+    #
     # ==== Examples
     # For bar charts
     #    bc.max_value 5 # 5 will be used to calculate the relative encoding values
@@ -144,7 +148,7 @@ module GoogleChart
         raise "Invalid max value for this chart type"
       end
     end
-    
+
     # Adds a background or chart fill. Call this option twice if you want both a background and a chart fill
     # [+bg_or_c+] Can be one of <tt>:background</tt> or <tt>:chart</tt> depending on the kind of fill requested
     # [+type+] Can be one of <tt>:solid</tt>, <tt>:gradient</tt> or <tt>:stripes</tt>
@@ -157,7 +161,7 @@ module GoogleChart
     # For <tt>:gradient</tt> type
     # * An <tt>:angle</tt>, which is the angle of the gradient between 0(horizontal) and 90(vertical)
     # * A <tt>:color</tt> option which is a 2D array containing the colors and an offset each, which specifies at what point the color is pure where: 0 specifies the right-most chart position and 1 the left-most. e,g <tt>lc.fill :background, :gradient, :angle => 0,  :color => [['76A4FB',1],['ffffff',0]]</tt>
-    # 
+    #
     # For <tt>:stripes</tt> type
     # * An <tt>:angle</tt>, which is the angle of the stripe between 0(horizontal) and 90(vertical)
     # * A <tt>:color</tt> option which is a 2D array containing the colors and width value each, which must be between 0 and 1 where 1 is the full width of the chart. for e.g <tt>lc.fill :chart, :stripes, :angle => 90, :color => [ ['76A4FB',0.2], ['ffffff',0.2] ]</tt>
@@ -169,56 +173,56 @@ module GoogleChart
         @chart_fill = "c," + process_fill_options(type, options)
       end
     end
-    
+
     # Adds an axis to the graph. Not applicable for Pie Chart (GoogleChart::PieChart) or Venn Diagram (GoogleChart::VennDiagram)
-    # 
-    # [+type+] is a symbol which can be one of <tt>:x</tt>, <tt>:y</tt>, <tt>:right</tt>, <tt>:top</tt> 
+    #
+    # [+type+] is a symbol which can be one of <tt>:x</tt>, <tt>:y</tt>, <tt>:right</tt>, <tt>:top</tt>
     # [+options+] is a hash containing the options (see below)
-    # 
+    #
     # ==== Options
     # Not all the options are mandatory.
     # [<tt>:labels</tt>] An array containing the labels for the axis
     # [<tt>:positions</tt>] An Array containing the positions for the labels
     # [<tt>:range</tt>] An array containing 2 elements, the start value and end value
-    # 
+    #
     # axis styling options have to be specified as follows
     # [<tt>:color</tt>] Hexadecimal RGB value for the color to represent the data for the axis labels
     # [<tt>:font_size</tt>] Font size of the labels in pixels
     # [<tt>:alignment</tt>] can be one of <tt>:left</tt>, <tt>:center</tt> or <tt>:right</tt>
-    # 
+    #
     # ==== Examples
     #     lc.axis :y, :range => [0,6], :color => 'ff00ff', :font_size => 16, :alignment => :center
-    #       
+    #
     def axis(type, options = {})
-      raise "Illegal axis type" unless [:x, :y, :right, :top].member?(type)          
+      raise "Illegal axis type" unless [:x, :y, :right, :top].member?(type)
       @axis << [type, options]
     end
-    
+
     # Adds a grid to the graph. Applicable only for Line Chart (GoogleChart::LineChart) and Scatter Chart (GoogleChart::ScatterChart)
     #
-    # [+options+] is a hash containing the options (see below) 
+    # [+options+] is a hash containing the options (see below)
     #
     # === Options
     # [<tt>:xstep</tt>] X axis step size
     # [<tt>:ystep</tt>] Y axis step size
     # [<tt>:length_segment</tt> (optional)] Length of the line segement. Useful with the :length_blank value to have dashed lines
     # [<tt>:length_blank</tt> (optional)] Length of the blank segment. use 0 if you want a solid grid
-    # 
+    #
     # === Examples
     #     lc.grid :x_step => 5, :y_step => 5, :length_segment => 1, :length_blank => 0
-    #        
+    #
     def grid(options={})
       @grid_str = "#{options[:x_step].to_f},#{options[:y_step].to_f}"
       if options[:length_segment] or options[:length_blank]
         @grid_str += ",#{options[:length_segment].to_f},#{options[:length_blank].to_f}"
       end
     end
-    
+
     # Defines a horizontal or vertical range marker. Applicable for line charts and vertical charts
     #
     # [+alignment+] can be <tt>:horizontal</tt> or <tt>:vertical</tt>
     # [+options+] specifies the color, start point and end point
-    # 
+    #
     # ==== Options
     # [<tt>:color</tt>] RRGGBB hex value for the color of the range marker
     # [<tt>:start_point</tt>]  position on the x-axis/y-axis at which the range starts where 0.00 is the left/bottom and 1.00 is the right/top
@@ -227,18 +231,18 @@ module GoogleChart
     # ==== Examples
     #     lc.range_marker :horizontal, :color => 'E5ECF9', :start_point => 0.1, :end_point => 0.5
     #     lc.range_marker :vertical, :color => 'a0bae9', :start_point => 0.1, :end_point => 0.5
-    def range_marker(alignment, options={}) 
+    def range_marker(alignment, options={})
       raise "Invalid alignment specified" unless [:horizontal, :vertical].member?(alignment)
       str = (alignment == :horizontal ) ? "r" : "R"
       str += ",#{options[:color]},0,#{options[:start_point]},#{options[:end_point]}"
-      @markers << str 
+      @markers << str
     end
 
     # Defines a shape marker. Applicable for line charts and scatter plots
     #
     # [+type+] can be <tt>:arrow</tt>, <tt>:cross</tt>, <tt>:diamond</tt>, <tt>:circle</tt>, <tt>:square</tt>, <tt>:vline_segment</tt>, <tt>:vline_full</tt>, <tt>:hline_full</tt>, <tt>:x</tt>
     # [+options+] specifies the color, data set index, data point index and size in pixels
-    # 
+    #
     # ==== Options
     # [<tt>:color</tt>] RRGGBB hex value for the color of the range marker
     # [<tt>:data_set_index</tt>]  the index of the line on which to draw the marker. This is 0 for the first data set, 1 for the second and so on.
@@ -291,7 +295,7 @@ module GoogleChart
         @markers << "b,#{color},#{start_index},#{end_index},0"
       end
     end
-    
+
     protected
 
     def prepare_params
@@ -301,7 +305,7 @@ module GoogleChart
       set_colors
       set_fill_options
       add_axis unless @axis.empty?
-      add_grid  
+      add_grid
       add_data
       add_line_styles unless @line_styles.empty?
       set_bar_width_spacing_options if @bar_width_spacing_options
@@ -310,7 +314,7 @@ module GoogleChart
       add_legend(@labels) if show_legend
       add_title  if chart_title.to_s.length > 0
     end
-    
+
     def process_fill_options(type, options)
       case type
       when :solid
@@ -320,45 +324,45 @@ module GoogleChart
       when :stripes
         "ls,#{options[:angle]}," + options[:color].collect { |o| "#{o.first},#{o.last}" }.join(",")
       end
-      
+
     end
-    
+
     def set_type
       params.merge!({:cht => chart_type})
     end
-    
+
     def set_size
       params.merge!({:chs => chart_size})
     end
-    
+
     def set_colors
       params.merge!({:chco => @colors.collect{|c| c.downcase}.join(",")  }) if @colors.size > 0
     end
-    
+
     def set_fill_options
       fill_opt = [@background_fill, @chart_fill].compact.join("|")
       params.merge!({:chf => fill_opt}) if fill_opt.length > 0
     end
-    
+
     def add_labels(labels)
-      params.merge!({:chl => labels.collect{|l| l.to_s}.join("|") }) if self.show_labels 
-    end                
-    
+      params.merge!({:chl => labels.collect{|l| l.to_s}.join("|") }) if self.show_labels
+    end
+
     def add_legend(labels)
       params.merge!({:chdl => labels.collect{ |l| l.to_s}.join("|")})
     end
-    
+
     def add_title
       params.merge!({:chtt => chart_title})
       params.merge!({:chts => title_color}) if title_color
       params.merge!({:chts => "#{title_color},#{title_font_size}"}) if title_color and title_font_size
     end
-    
-    def add_axis          
+
+    def add_axis
       chxt = []
       chxl = []
       chxp = []
-      chxr = []                       
+      chxr = []
       chxs = []
       # Process params
       @axis.each_with_index do |axis, idx|
@@ -373,24 +377,24 @@ module GoogleChart
         when :right
           chxt << "r"
         end
-        
+
         # Axis labels
         axis_opts = axis.last
-        
+
         if axis_opts[:labels]
           chxl[idx] = "#{idx}:|" + axis_opts[:labels].join("|")
         end
-        
+
         # Axis positions
         if axis_opts[:positions]
           chxp[idx] = "#{idx}," + axis_opts[:positions].join(",")
         end
-        
+
         # Axis range
         if axis_opts[:range]
-          chxr[idx] = "#{idx},#{axis_opts[:range].first},#{axis_opts[:range].last}"                
+          chxr[idx] = "#{idx},#{axis_opts[:range].first},#{axis_opts[:range].last}"
         end
-        
+
         # Axis Styles
         if axis_opts[:color] or axis_opts[:font_size] or axis_opts[:alignment]
           if axis_opts[:alignment]
@@ -400,7 +404,7 @@ module GoogleChart
                         when :left
                           -1
                         when :right
-                          1 
+                          1
                         else
                           nil
                         end
@@ -408,7 +412,7 @@ module GoogleChart
           chxs[idx] = "#{idx}," + [axis_opts[:color], axis_opts[:font_size], alignment].compact.join(",")
         end
       end
-      
+
       # Add to params hash
       params.merge!({ :chxt => chxt.join(",") })          unless chxt.empty?
       params.merge!({ :chxl => chxl.compact.join("|") })  unless chxl.compact.empty?
@@ -416,7 +420,7 @@ module GoogleChart
       params.merge!({ :chxr => chxr.compact.join("|") })  unless chxr.compact.empty?
       params.merge!({ :chxs => chxs.compact.join("|") })  unless chxs.compact.empty?
     end
-    
+
     def add_grid
       params.merge!({ :chg => @grid_str }) if @grid_str
     end
@@ -431,7 +435,7 @@ module GoogleChart
     def set_bar_width_spacing_options
       params.merge!({:chbh => @bar_width_spacing_options})
     end
-    
+
     def add_markers
       params.merge!({:chm => @markers.join("|")})
     end
@@ -450,7 +454,7 @@ module GoogleChart
       end
       params.merge!({:chd => converted_data})
     end
-    
+
     def encode_data(values, max_value=nil)
       case data_encoding
       when :simple
@@ -463,14 +467,14 @@ module GoogleChart
         raise "Illegal Encoding Specified"
       end
     end
-    
+
     def simple_encode(values, max_value=nil)
       alphabet_length = 61
       max_value = values.max unless max_value
 
-      chart_data = values.collect do |val|              
+      chart_data = values.collect do |val|
         if val.to_i >=0
-          if max_value == 0  
+          if max_value == 0
             SIMPLE_ENCODING[0]
           else
             SIMPLE_ENCODING[(alphabet_length * val / max_value).to_i]
@@ -479,10 +483,10 @@ module GoogleChart
           "_"
         end
       end
-      
+
       return chart_data.join('')
     end
-    
+
     def text_encode(values, max_value=nil)
       max_value = values.max unless max_value
       values.inject("") { |sum, v|
@@ -493,7 +497,7 @@ module GoogleChart
         end
       }.chomp(",")
     end
-    
+
     def extended_encode(values, max_value)
       max_value = values.max unless max_value
       values.collect { |v|
@@ -504,15 +508,15 @@ module GoogleChart
         end
       }.join('')
     end
-    
+
     def join_encoded_data(encoded_data)
       encoded_data.join((self.data_encoding == :simple or self.data_encoding == :extended) ? "," : "|")
     end
-    
+
     def max_data_value
       @max_data or @data.flatten.max
     end
-    
+
     def max_x_value
       @max_x or x_data.flatten.max
     end
@@ -531,6 +535,6 @@ module GoogleChart
       @data.collect do |series|
         series.collect { |val| val.last }
       end
-    end        
+    end
   end
 end
